603307f5af08a5c61f90ba5e88c59181c76319af
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-05 19:38:16 +0530

    Update CountryNotValidException.java

--- CountryNotValidException.java
+++ CountryNotValidException.java
@@ -1,5 +1,6 @@
 package in.co.capgemini;
 
+//Class CountryNotValidException for throwing userdefine exception
 public class CountryNotValidException extends Exception {
 	public CountryNotValidException(String message) {
 		super(message);
