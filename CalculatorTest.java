610514f7b4c129582e4d3564fd3fba38538dc656
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-05 19:36:27 +0530

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -2,26 +2,31 @@ package in.co.capgemini;
 
 public class CalculatorTest {
 
+	//Add method for adding 2 numbers
 	public int add(int i, int j) {
 		// TODO Auto-generated method stub
 		return i+j;
 	}
 
+	//subtract method for subtracting 2 numbers
 	public int sub(int i, int j) {
 		// TODO Auto-generated method stub
 		return i-j;
 	}
 
+	//Multiply method for multiplying 2 numbers
 	public int multiply(int i, int j) {
 		// TODO Auto-generated method stub
 		return j*i;
 	}
 
+	//Division method for dividing 2 numbers
 	public int divide(int i, int j) {
 		// TODO Auto-generated method stub
 		return i/j;
 	}
 
+	//Modulus method 
 	public int mod(int i, int j) {
 		// TODO Auto-generated method stub
 		return i%j;
