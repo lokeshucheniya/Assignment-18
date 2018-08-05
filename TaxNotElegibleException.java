3cf3779055050af2f0a01b274398f4c57d001e97
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-05 19:45:32 +0530

    Update TaxNotElegibleException.java

--- TaxNotElegibleException.java
+++ TaxNotElegibleException.java
@@ -1,5 +1,6 @@
 package in.co.capgemini;
 
+//User define exception TaxNotElegibleException
 public class TaxNotElegibleException extends Exception {
 	public TaxNotElegibleException(String message) {
 		super(message);
