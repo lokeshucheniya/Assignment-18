f580055cabea1a44147645618abbfef91341bc56
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-05 19:38:45 +0530

    Update EmployeeNameInvalidException.java

--- EmployeeNameInvalidException.java
+++ EmployeeNameInvalidException.java
@@ -1,5 +1,6 @@
 package in.co.capgemini;
 
+//Class EmployeeNameInvalidException for throwing userdefine exception
 public class EmployeeNameInvalidException extends Exception {
 	public EmployeeNameInvalidException(String message) {
 		super(message);
